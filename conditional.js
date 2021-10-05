a5f8742a3b9375648910e9e5f23def19847ad43f
kunvar13 <kunvar13@gmail.com>
2021-10-05 09:26:11 -0600

    more examples

--- conditional.js
+++ conditional.js
@@ -9,7 +9,7 @@ if (cold) {
   console.log("Make sure you pick out a scarf!");
 }
 
-console.log("Now you're ready to go outside!"); */
+console.log("Now you're ready to go outside!"); 
 
 const temperature = -5;
 
@@ -21,4 +21,11 @@ if (temperature < 0) {
   console.log("Short sleeves are fine.");
 }
 
-console.log("Now you're ready to go outside!");
+console.log("Now you're ready to go outside!");*/
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
